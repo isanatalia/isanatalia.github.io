ffcd47d4d56ddd34f9945c0217b7129792058802
isanatalia <nataliamagalhaesd@gmail.com>
2022-11-10 10:43:42 -0300

    My page.V15

--- app.js
+++ app.js
@@ -7,20 +7,18 @@ const allSections = document.querySelectorAll('.main-content');
 function PageTransitions(){
     //Button click active class
     for(let i=0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', function(){
+        
+        [i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
             this.className += 'active-btn';            
-        } )
+        })
     }
 
     // Sections Active class
     allSections.addEventListener('click', (e) =>{
-        console.log(e.target);
-
-
-    })
-
+        const id = e.target.dataset.id;
+   })
 }
 
 PageTransitions();
